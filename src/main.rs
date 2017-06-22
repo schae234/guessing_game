f694d81339c140ded739e6498b94f2f2d524c650
Rob <schae234@umn.edu>
2017-06-22 12:32:58 -0500

    implemented guessing game

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess a number between 1 and 100");
+
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("The secret number is {}",secret_number);
+
+    loop {
+        println!("Please input your guess");
+        let mut guess = String::new();
+   
+        // read a line in from Stdin 
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
+    
+        // convert guess to a u32
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed {}",guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal   => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
+
+}
